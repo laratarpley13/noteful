cb950904071e7aa4ca36b5543a403465c06528c0
laratarpley13 <ltarp13@gmail.com>
2021-01-17 13:41:28 -0600

    changed error message for error boundary

--- src/appError.js
+++ src/appError.js
@@ -13,7 +13,7 @@ export default class AppError extends Component {
     render() {
         if (this.state.hasError) {
             return (
-                <h2>Could not display this currency.</h2>
+                <h2>Could not display this web page.</h2>
             );
         }
         return this.props.children;
